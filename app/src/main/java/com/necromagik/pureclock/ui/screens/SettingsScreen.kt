38a72f9dbc864ddad4838c5be198f55cf39df2c1
NecroMagik <zn.necromagik@gmail.com>
2026-08-02 14:16:46 +0300

    Меня победили долбаные китыйцы

--- app/src/main/java/com/necromagik/pureclock/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/necromagik/pureclock/ui/screens/SettingsScreen.kt
@@ -43,7 +43,7 @@ import kotlinx.coroutines.launch
 // ============================================================================
 // ВЕРСИЯ ПРИЛОЖЕНИЯ (МЕНЯТЬ ЗДЕСЬ)
 // ============================================================================
-private const val APP_VERSION = "Pure_1.12 !pre-release!"
+private const val APP_VERSION = "Pure_1.13 !pre-release!"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
